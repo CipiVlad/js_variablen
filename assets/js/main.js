66c7c99b10b40c02c4924e68569c2eb33fd4829d
CipiVlad <supercoder.cipi@gmail.com>
2022-04-13 23:55:23 +0200

    js-variablen

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// let carName = 'BMW';
+// console.log(carName);
+
+// let x = 150;
+// let y = 50;
+// let z = x + y;
+// console.log(x, y, z);
+
+// let firstName = 'John';
+// let lastName = 'Doe';
+// const age = 35;
+
+// console.log('My name is' + " " + firstName + " "
+//     + lastName + " . and I am " + age + ".");
+
+
+
+// Unterschied zwischen let, const und var
+
+// var z = 34;
+// z = 67;
+// console.log(z);
+
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+const y = 34;
+y = 67;
+console.log(y);
